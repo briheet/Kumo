af204e4f92e32ec32574022908a39f8f3b56ab0b
briheet <briheetyadav@gmail.com>
2025-09-29 17:25:12 +0530

    init: flake
    This is the init for kumo

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Kumo dependency flake wohoooo";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+
+    supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs { inherit system; }
+    );
+
+  in
+  {
+    devShells = forAllSystems (system:
+      let
+
+        pkgs = nixpkgsFor.${system};
+
+      in {
+
+        default = pkgs.mkShell {
+
+          buildInputs = [
+            pkgs.go_1_25
+            pkgs.fish
+          ];
+
+          shellHook = ''
+            export SHELL=${pkgs.fish}/bin/fish
+            exec ${pkgs.fish}/bin/fish
+          '';
+
+        };
+      }
+
+    );
+  };
+
+}
