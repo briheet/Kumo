c778bab3d59c9df94c802159dc01bc7b570863d5
briheet <briheetyadav@gmail.com>
2025-10-05 10:24:52 +0530

    upd: service flake

--- flake.nix
+++ flake.nix
@@ -1,46 +1,79 @@
 {
-  description = "Kumo dependency flake wohoooo";
-
+  description = "Kumo flake for shell and services";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
+
+    northwind.url = "github:pthom/northwind_psql";
+    northwind.flake = false;
   };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.process-compose-flake.flakeModule
+      ];
+      perSystem = { self', pkgs, config, lib, ... }: {
+        # `process-compose.foo` will add a flake package output called "foo".
+        # Therefore, this will add a default package that you can build using
+        # `nix build` and run using `nix run`.
+        process-compose."default" = { config, ... }:
+          let
+            dbName = "sample";
+          in
+          {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+            ];
 
-  outputs = { self, nixpkgs }:
-  let
+            services.postgres."pg1" = {
+              enable = true;
+              initialDatabases = [
+                {
+                  name = dbName;
+                  schemas = [ "${inputs.northwind}/northwind.sql" ];
+                }
+              ];
+            };
 
-    supportedSystems = [ "x86_64-linux" "aarch64-darwin" ];
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    nixpkgsFor = forAllSystems (system:
-      import nixpkgs { inherit system; }
-    );
+            settings.processes.pgweb =
+              let
+                pgcfg = config.services.postgres.pg1;
+              in
+              {
+                environment.PGWEB_DATABASE_URL = pgcfg.connectionURI { inherit dbName; };
+                command = pkgs.pgweb;
+                depends_on."pg1".condition = "process_healthy";
+              };
+            settings.processes.test = {
+              command = pkgs.writeShellApplication {
+                name = "pg1-test";
+                runtimeInputs = [ config.services.postgres.pg1.package ];
+                text = ''
+                  echo 'SELECT version();' | psql -h 127.0.0.1 ${dbName}
+                '';
+              };
+              depends_on."pg1".condition = "process_healthy";
+            };
+          };
 
-  in
-  {
-    devShells = forAllSystems (system:
-      let
+        # Put nats here
+        
 
-        pkgs = nixpkgsFor.${system};
-
-      in {
-
-        default = pkgs.mkShell {
-
-          buildInputs = [
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            config.process-compose."default".services.outputs.devShell
+          ];
+          nativeBuildInputs = [
             pkgs.go_1_25
             pkgs.gopls
             pkgs.golangci-lint-langserver
-            # pkgs.fish
+            pkgs.delve
           ];
-
-          shellHook = ''
-            # export SHELL=${pkgs.fish}/bin/fish
-            # exec ${pkgs.fish}/bin/fish
-          '';
-
         };
-      }
-
-    );
-  };
-
+      };
+    };
 }
